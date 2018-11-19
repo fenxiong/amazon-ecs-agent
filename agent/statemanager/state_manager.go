592117febaaa6d9501fc0d9b33308a0b1aa7b9da
fenxiong <fenxiong@amazon.com>
2018-11-18 19:40:57 -0800

    test save fix

--- agent/statemanager/state_manager.go
+++ agent/statemanager/state_manager.go
@@ -194,10 +194,10 @@ func AddSaveable(name string, saveable Saveable) Option {
 // Save triggers a save to file, though respects a minimum save interval to wait
 // between saves.
 func (manager *basicStateManager) Save() error {
-	defer metrics.MetricsEngineGlobal.RecordStateManagerMetric("SAVE")()
 	manager.saveTimesLock.Lock()
 	defer manager.saveTimesLock.Unlock()
 	if time.Since(manager.lastSave) >= minSaveInterval {
+		defer metrics.MetricsEngineGlobal.RecordStateManagerMetric("SAVE")()
 		// we can just save
 		err := manager.ForceSave()
 		manager.lastSave = time.Now()
