cc0af98c190881eef01533fd3d6f40e0b7296002
fenxiong <fenxiong@amazon.com>
2019-07-16 20:10:24 -0700

    Various small fixes for log router.
    1. Fix acs model case mismatch.
    2. Fix socket address of log sender container.
    3. Fix log config of log sender container.

--- agent/api/task/task.go
+++ agent/api/task/task.go
@@ -761,7 +761,7 @@ func (task *Task) initializeLogRouterResource(config *config.Config, resourceFie
 			}
 
 			logRouterResource = logrouter.NewLogRouterResource(config.Cluster, task.Arn, task.Family+":"+task.Version,
-				ec2InstanceID, config.DataDir, logRouter.Type, logRouter.EnableECSLogMetaData, containerToLogOptions)
+				ec2InstanceID, config.DataDir, logRouter.Type, logRouter.EnableECSLogMetadata, containerToLogOptions)
 			task.AddResource(logrouter.ResourceName, logRouterResource)
 			container.BuildResourceDependency(logRouterResource.GetName(), resourcestatus.ResourceCreated,
 				apicontainerstatus.ContainerCreated)
